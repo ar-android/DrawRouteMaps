3b0afdf9eaf817ce287fc767572e8b0f5ae2d648
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2018-11-05 17:08:10 +0000

    updated FetchUrl to include google key param

--- drawroutemap/src/main/java/com/ahmadrosid/lib/drawroutemap/FetchUrl.java
+++ drawroutemap/src/main/java/com/ahmadrosid/lib/drawroutemap/FetchUrl.java
@@ -11,11 +11,12 @@ import com.google.android.gms.maps.model.LatLng;
  * @Web http://ahmadrosid.com
  */
 public class FetchUrl {
-    public static String getUrl(LatLng origin, LatLng dest) {
+    public static String getUrl(LatLng origin, LatLng dest, String google_maps_key) {
         String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
         String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
         String sensor = "sensor=false";
-        String parameters = str_origin + "&" + str_dest + "&" + sensor;
+	    String key = "key=" + google_maps_key;
+        String parameters = str_origin + "&" + str_dest + "&" + sensor + "&" + key;
         String output = "json";
         return "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
     }
